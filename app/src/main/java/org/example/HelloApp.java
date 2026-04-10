2fb4a2e8a347916072472a730f4c9acfc820b95a
penchalahanushboddu-arch <penchalahanushboddu@gmail.com>
2026-04-10 15:26:02 +0530

    Update HelloApp.java

--- app/src/main/java/org/example/HelloApp.java
+++ app/src/main/java/org/example/HelloApp.java
@@ -2,6 +2,15 @@ package org.example;
 
 class HelloApp{
     public static void main(String [] args){
+        String name;
+
+        // Check if command-line argument is provided
+        if (args.length > 0) {
+            name = args[0];   // take first argument
+        } else {
+            name = "World";  // default value
+        }
+        System.out.println("Hello, " + name + "!");
 
     }
 }
