4f89ce6c7ba75eb1173afd6341ee78bf4aa77781
penchalahanushboddu-arch <penchalahanushboddu@gmail.com>
2026-04-10 16:36:24 +0530

    Update HelloApp.java

--- app/src/main/java/org/example/HelloApp.java
+++ app/src/main/java/org/example/HelloApp.java
@@ -2,14 +2,7 @@ package org.example;
 
 class HelloApp{
     public static void main(String [] args){
-        String name;
-        if (args.length > 0) {
-            name = args[0];
-        }
-        else {
-            name = "World";
-        }
-
+        String name = (args.length > 0) ? args[0] : "World";
         System.out.println("Hello, " + name + "!");
 
     }
