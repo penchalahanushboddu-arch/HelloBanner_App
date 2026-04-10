a34cc01526f21aaf5c82f784384bf8268f883243
penchalahanushboddu <penchalahanushboddu@gmail.com>
2026-04-10 15:41:44 +0530

    initial nellore commit

--- app/src/main/java/org/example/HelloApp.java
+++ app/src/main/java/org/example/HelloApp.java
@@ -2,12 +2,20 @@ package org.example;
 
 class HelloApp{
     public static void main(String [] args){
-        String names;
+        String names = "";
 
-        if (args.length > 0) {
-            names = String.join(", ", args);  // join all names with comma
+        if (args.length == 0) {
+            names = "World";
         } else {
-            names = "World";  // default
+            boolean first = true;
+
+            for (String name : args) {
+                if (!first) {
+                    names += ", ";
+                }
+                names += name;
+                first = false;
+            }
         }
 
         System.out.println("Hello, " + names + "!");
