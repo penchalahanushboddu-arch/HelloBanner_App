0b7d3207f16108ff1d7056a4bb68845ed9751ceb
penchalahanushboddu-arch <penchalahanushboddu@gmail.com>
2026-04-10 16:27:10 +0530

    Update HelloApp.java

--- app/src/main/java/org/example/HelloApp.java
+++ app/src/main/java/org/example/HelloApp.java
@@ -1,16 +1,7 @@
 package org.example;
 
-class HelloApp{
-    public static void main(String [] args){
-        String names;
-
-        if (args.length > 0) {
-            names = String.join(", ", args);  // join all names with comma
-        } else {
-            names = "World";  // default
-        }
-
-        System.out.println("Hello, " + names + "!");
-
+public class Main {
+    static void main() {
+        System.out.println("Hello World !");
     }
 }
